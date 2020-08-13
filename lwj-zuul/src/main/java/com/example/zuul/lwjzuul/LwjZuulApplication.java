7f2799c188bec3adf80d8bda41a8e9c8aea98b5d
weijie110 <weijie110@qq.com>
2020-08-13 09:37:47 +0800

    wejie110

--- lwj-zuul/src/main/java/com/example/zuul/lwjzuul/LwjZuulApplication.java
+++ lwj-zuul/src/main/java/com/example/zuul/lwjzuul/LwjZuulApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class LwjZuulApplication {
-
+//试试
 	public static void main(String[] args) {
 		SpringApplication.run(LwjZuulApplication.class, args);
 	}
